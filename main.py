c6218b3420664c56f391912c1ee5790c42309576
Diego Gomes <eng.diegogomes@gmail.com>
2025-02-12 09:06:07 -0300

    Criado o código exemplo main
    Criado o código exemplo main

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+Script simples de Hello World para portifólio
+"""
+
+def main():
+    "Função principal que imprime uma mensagem de boas-vindas."
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
